32abbc544f064ad5fc4b36a45bcd5c39d511a2f5
ivanlasic23 <ivanlasic23@yahoo.com>
2022-02-03 12:46:58 +0000

    Add questions about bands

---
+++ assets/js/questions.js
@@ -0,0 +1,55 @@
+const quizQuestionsBands [
+    {
+        question : "Who was the lead singer for Queen?",
+        a: "Robert Plant",
+        b: "Freddie Mercury",
+        c: "George Michael",
+        d: "Seal",
+        correct: "b",
+    },
+
+    {
+        question: "Where does the popular band A-ha come from?",
+        a: "Sweden",
+        b: "Denmark",
+        c: "Norway",
+        d: "England",
+        correct: "c",
+    },
+
+    {
+        question: "What was Prince' best selling album of all time?",
+        a: "Purple Rain",
+        b: "1999",
+        C: "Around the World in a Day",
+        d: "Emancipation",
+        correct: "a",
+    },
+
+    {
+        question: "How many records have Boni Jovi sell worldwide?",
+        a: "40 million",
+        b: "50 million",
+        c: "74 million",
+        d: "130 million",
+        correct: "d",
+    },
+
+    {
+        question: "What are the names of Van Halen brothers?",
+        a: "Johnnie & Freddie",
+        b: "Alex & Eddie",
+        c: "Ben & Jerry",
+        d: "Jordan & Larry",
+        correct: "b",
+    },
+
+    {
+        question: "Where was band U2 formed?",
+        a: "London",
+        b: "Dublin",
+        c: "Cork",
+        d: "Liverpool",
+        correct: "b",
+    }
+]

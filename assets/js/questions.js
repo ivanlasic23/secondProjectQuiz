ff401f2ef0069ea4055220577dd32a48803fb297
ivanlasic23 <ivanlasic23@yahoo.com>
2022-02-03 13:36:01 +0000

    Add more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,4 @@
-const quizQuestionsBands [
+const quizQuestionsBands =[
     {
         question : "Who was the lead singer for Queen?",
         a: "Robert Plant",
@@ -51,5 +51,52 @@ const quizQuestionsBands [
         c: "Cork",
         d: "Liverpool",
         correct: "b",
+    }
+]
+
+const quizQuestionsLyrics = [
+    {
+        question: "Is the the real life __________?",
+        a: "Is this just magic",
+        b: "Is this just fantasy",
+        c: "Is this just mercury",
+        d: "Is this just landslide",
+        correct: "b",
+    },
+
+    {
+        question: "Ah, can't you see what I mean__________?",
+        a: "Ah, might as well jump",
+        b: "Might as well lump",
+        c: "Might as well stop",
+        d: "None of the above",
+        correct: "a",
+    },
+
+    {
+        question: "I never meant to cause you any sorrow__________?",
+        a: "I never meant to cause you and gain",
+        b: "I never wanted to be your main",
+        c: "I never meant to cause you any pain",
+        d: "I never ever want to see you again",
+        correct: "c",
+    },
+
+    {
+        question: "With or withour you_____________?",
+        a: "I can't live",
+        b: "I can't breathe",
+        c: "I can't be",
+        d: "I can't even",
+        correct: "a",
+    },
+
+    {
+        question: "We've got each other and that's a lot for love We'll give it a ____?",
+        a: "jump",
+        b: "life",
+        c: "shot",
+        d: "magic",
+        correct: "c",
     }
 ]

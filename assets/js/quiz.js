d4362ef4564d66f360d99813c577287d693ed4ed
ivanlasic23 <ivanlasic23@yahoo.com>
2022-03-02 01:13:25 +0000

    Test javascript with JS Hint and clean it up

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,113 +1,111 @@
 const quizQuestionsBands = [{
-    question: "Who was the lead singer for Queen?",
-    choice1: "Robert Plant",
-    choice2: "Freddie Mercury",
-    choice3: "George Michael",
-    choice4: "Seal",
-    correct: 2,
-},
+        question: "Who was the lead singer for Queen?",
+        choice1: "Robert Plant",
+        choice2: "Freddie Mercury",
+        choice3: "George Michael",
+        choice4: "Seal",
+        correct: 2,
+    },
 
-{
-    question: "Where does the popular band A-ha come from?",
-    choice1: "Sweden",
-    choice2: "Denmark",
-    choice3: "Norway",
-    choice4: "England",
-    correct: 3,
-},
+    {
+        question: "Where does the popular band A-ha come from?",
+        choice1: "Sweden",
+        choice2: "Denmark",
+        choice3: "Norway",
+        choice4: "England",
+        correct: 3,
+    },
 
-{
-    question: "What was Prince' best selling album of all time?",
-    choice1: "Purple Rain",
-    choice2: "1999",
-    choice3: "Around the World in a Day",
-    choice4: "Emancipation",
-    correct: 1,
-},
+    {
+        question: "What was Prince' best selling album of all time?",
+        choice1: "Purple Rain",
+        choice2: "1999",
+        choice3: "Around the World in a Day",
+        choice4: "Emancipation",
+        correct: 1,
+    },
 
-{
-    question: "How many records have Boni Jovi sold worldwide?",
-    choice1: "40 million",
-    choice2: "50 million",
-    choice3: "74 million",
-    choice4: "130 million",
-    correct: 4,
-},
+    {
+        question: "How many records have Boni Jovi sold worldwide?",
+        choice1: "40 million",
+        choice2: "50 million",
+        choice3: "74 million",
+        choice4: "130 million",
+        correct: 4,
+    },
 
-{
-    question: "What are the names of Van Halen brothers?",
-    choice1: "Johnnie & Freddie",
-    choice2: "Alex & Eddie",
-    choice3: "Ben & Jerry",
-    choice4: "Jordan & Larry",
-    correct: 2,
-},
+    {
+        question: "What are the names of Van Halen brothers?",
+        choice1: "Johnnie & Freddie",
+        choice2: "Alex & Eddie",
+        choice3: "Ben & Jerry",
+        choice4: "Jordan & Larry",
+        correct: 2,
+    },
 
-{
-    question: "Where was band U2 formed?",
-    choice1: "London",
-    choice2: "Dublin",
-    choice3: "Cork",
-    choice4: "Liverpool",
-    correct: 2,
-},
+    {
+        question: "Where was band U2 formed?",
+        choice1: "London",
+        choice2: "Dublin",
+        choice3: "Cork",
+        choice4: "Liverpool",
+        correct: 2,
+    },
 
-{
-    question: "Is this just ___?",
-    choice1: "magic",
-    choice2: "fantasy",
-    choice3: "mercury",
-    choice4: "landslide",
-    correct: 2,
-},
+    {
+        question: "Is this just ___?",
+        choice1: "magic",
+        choice2: "fantasy",
+        choice3: "mercury",
+        choice4: "landslide",
+        correct: 2,
+    },
 
-{
-    question: "Ah, can't you see what I mean___?",
-    choice1: "Ah, might as well jump",
-    choice2: "Might as well lump",
-    choice3: "Might as well stop",
-    choice4: "None of the above",
-    correct: 1,
-},
+    {
+        question: "Ah, can't you see what I mean___?",
+        choice1: "Ah, might as well jump",
+        choice2: "Might as well lump",
+        choice3: "Might as well stop",
+        choice4: "None of the above",
+        correct: 1,
+    },
 
-{
-    question: "I never meant to cause you any ___?",
-    choice1: "gain",
-    choice2: "main",
-    choice3: "pain",
-    choice4: "fame",
-    correct: 3,
-},
+    {
+        question: "I never meant to cause you any ___?",
+        choice1: "gain",
+        choice2: "main",
+        choice3: "pain",
+        choice4: "fame",
+        correct: 3,
+    },
 
-{
-    question: "With or withour you____?",
-    choice1: "I can't live",
-    choice2: "I can't breathe",
-    choice3: "I can't be",
-    choice4: "I can't even",
-    correct: 1,
-},
+    {
+        question: "With or withour you____?",
+        choice1: "I can't live",
+        choice2: "I can't breathe",
+        choice3: "I can't be",
+        choice4: "I can't even",
+        correct: 1,
+    },
 
-{
-    question: "We've got each other and that's a lot for love We'll give it a ____?",
-    choice1: "jump",
-    choice2: "life",
-    choice3: "shot",
-    choice4: "magic",
-    correct: 3,
-},
-{
-    question: "Take on me,____",
-    choice1: "Take me on",
-    choice2: "Take me out",
-    choice3: "Take me there",
-    choice4: "Take me take me",
-    correct: 1,
-}
-]
+    {
+        question: "We've got each other and that's a lot for love We'll give it a ____?",
+        choice1: "jump",
+        choice2: "life",
+        choice3: "shot",
+        choice4: "magic",
+        correct: 3,
+    },
+    {
+        question: "Take on me,____",
+        choice1: "Take me on",
+        choice2: "Take me out",
+        choice3: "Take me there",
+        choice4: "Take me take me",
+        correct: 1,
+    }
+];
 
-
-let answer = document.getElementsByClassName("answer-div");
 const quiz = document.getElementById("quiz");
 const questions = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("answer-value"));
@@ -138,7 +136,7 @@ function startBandQuiz() {
 function showQuestionBand() {
 
     if (nextQuestion.length === 0) {
-        whichMessageToShow()      
+        whichMessageToShow();
     }
 
     currentQ++;
@@ -149,9 +147,9 @@ function showQuestionBand() {
     questions.innerText = currentQuestions.question;
 
     choices.forEach(choice => {
-        const number = choice.dataset['number'];
+        const number = choice.dataset.number;
         choice.innerText = currentQuestions['choice' + number];
-    })
+    });
 
     //Make sure we dont repeat the same question
     nextQuestion.splice(questionIndex, 1);
@@ -168,7 +166,7 @@ choices.forEach(choice => {
 
         acceptingAnswers = true;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset.number;
 
         let classToApply;
 
@@ -176,6 +174,7 @@ choices.forEach(choice => {
             classToApply = "correct";
             incrementScore();
             this.innerText = "Correct";
+
         } else {
             classToApply = "incorrect";
             this.innerText = "Incorrect";
@@ -183,8 +182,7 @@ choices.forEach(choice => {
         selectedChoice.parentElement.classList.add(classToApply);
         //Give it time to show the correct answer and transition
         setTimeout(() => {
-            showQuestionBand()
-
+            showQuestionBand();
         }, 1500);
 
         // increment score
@@ -194,20 +192,19 @@ choices.forEach(choice => {
             currentScore.innerText = score;
 
         }
-    })
-})
+    });
+});
 
 // Which message will be shown upon quiz completion
-function whichMessageToShow(){
-    if(score==maxQ){
-        quiz.innerHTML=`<h1>Great Job! You are a true 80's fan</h1>
-        <a href="index.html" class="btn">Play Again</a>`
+function whichMessageToShow() {
+    if (score == maxQ) {
+        quiz.innerHTML = `<h1 class="you-scored">Great Job! You are a true 80's fan</h1>
+        <a href="index.html" class="btn btn-finish">Play Again</a>`;
+    } else if (score < maxQ) {
+        quiz.innerHTML = `<h1 class="you-scored">You have scored ${score} out of ${maxQ}. We think you should study more</h1>
+        <a href="study.html" class="btn btn-finish">Study</a>
+        <a href="index.html" class="btn btn-finish">Play Again</a>`;
     }
-    else if (score<maxQ){
-        quiz.innerHTML=`<h1>You have scored ${score} out of ${maxQ}. We think you should study more</h1>
-        <a href="study.html" class="btn">Study</a>
-        <a href="index.html" class="btn">Play Again</a>`
-    }    
 }
 
 startBandQuiz();
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 // Change color on main page on mouse over
+function crazyColor() {
+    const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+    var randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+    function namess() {
+        this.style.backgroundColor = randomColor();}
 
-document.getElementById("testing").addEventListener("mouseover", function () {
-    this.style.backgroundColor = randomColor()
-})
+    document.getElementsByClassName("main-quiz-area")[0].addEventListener("mouseover", namess)
+    
+}
+crazyColor();
 
+function turnOffCrazyColor() {
+    document.getElemenstByClassName("main-quiz-area")[0].classList.remove("main-quiz-area");
+    
+}
+
+let turnOffBtn = document.getElementById("turn-off");
+turnOffBtn.addEventListener("click", turnOffCrazyColor());
